6195eb362f26a8199cb1c57397e40d4341af3ceb
Arunev4wayne <153321600+Arunev4wayne@users.noreply.github.com>
2023-12-18 04:38:39 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string="mysql+pymysql://yyg7sar80ajo40rdx44l:pscale_pw_NtfWrHNH8yF0dxxssueZSQ0VoEpl7wBbDztIZLwpz8u@aws.connect.psdb.cloud/swd?charset=utf8mb4"
+<<<<<<< HEAD
+db_connection_string="mysql+pymysql://dsis161qa17e6ojjy41w:pscale_pw_7V9GBZruXMw8Kv2tZUtlStSdWMT3Vpz3osUthNCHVEp@eu-west.connect.psdb.cloud/swd?charset=utf8mb4"
 engine = create_engine(
  db_connection_string, 
  connect_args={
