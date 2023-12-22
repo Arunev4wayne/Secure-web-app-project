a7c288536b788bc4fa9fad9807ca6cc7b014e936
Arunev4wayne <153321600+Arunev4wayne@users.noreply.github.com>
2023-12-22 14:29:47 +0000

    DAtabase updated

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ app.secret_key = 'secretkey'
 
 db_config = {
     'host': 'aws.connect.psdb.cloud',
-    'user': 'kh6yqggeug8mgofnhzdt',
-    'password': 'pscale_pw_CFGAlOZ59RdixfEJ74gGEHYu2KpmDpgnutoU9dfewzf',
+    'user': '5w959nrvcrio4fz6jllh',
+    'password': 'pscale_pw_qzvPcVrLyBjz4jw45j1deLNbmlNPjL8AYScXIHnP8FA',,
     'database': 'swd',
 }
 
